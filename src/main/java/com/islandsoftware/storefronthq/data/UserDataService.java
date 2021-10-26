a6e35686c9f4a8ba6d71aba3ed68c16e0c694a58
Kelly Clauson <krclauson@gmail.com>
2021-10-26 08:48:08 -0400

    add user id

--- src/main/java/com/islandsoftware/storefronthq/data/UserDataService.java
+++ src/main/java/com/islandsoftware/storefronthq/data/UserDataService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class UserDataService {
@@ -13,7 +14,9 @@ public class UserDataService {
     private final Map<String, User> users = new HashMap<>();
 
     public void addUser(final User user) {
-        users.put(user.getUsername(), user);
+        UUID uuid = UUID.randomUUID();
+        user.setId(uuid.toString());
+        users.put(uuid.toString(), user);
     }
 
     public Collection<User> getUsers() {
--- src/main/java/com/islandsoftware/storefronthq/domain/User.java
+++ src/main/java/com/islandsoftware/storefronthq/domain/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
+    private String id;
     private String username;
     private String email;
     private String dob;
